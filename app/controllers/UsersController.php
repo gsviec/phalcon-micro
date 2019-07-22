2d819dd7b86f8bc3d796b737c3b71890ad5c0d44
Thien Tran <fcduythien@gmail.com>
2019-07-22 15:14:48 +0700

    Remove space

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -15,24 +15,21 @@ class UsersController extends ControllerBase
     public function add()
     {
     	$data = $this->parserDataRequest();
+   	if (!isset($data['email'])) {
+   		return  $this->respondError('You need provider email');
+   	}
+   	$user = Users::findFirstByEmail($data['email']);
+   	if (!$user) {
+   		$user = new Users();
+   		$data['password'] =  $this->security->hash($data['password']);
+   	}
 
-   		if (!isset($data['email'])) {
-   			return  $this->respondError('You need provider email');
-   		}
-   		$user = Users::findFirstByEmail($data['email']);
-   		if (!$user) {
-   			$user = new Users();
-   			$data['password'] =  $this->security->hash($data['password']);
-   		}
+   	if (!$user->save($data)) {
+   		foreach ($user->getMessages() as $m) {
+                	return  $this->respondWithError('Add user fall');
+        	}
+   	}
 
-   		if (!$user->save($data)) {
-   			
-   			foreach ($user->getMessages() as $m) {
-                //var_dump($m->getMessage());
-                return  $this->respondWithError('Add user fall');
-            }
-   		}
-
-   		return  $this->respondWithSuccess('Add user success');
+   	return  $this->respondWithSuccess('Add user success');
     }
 }
